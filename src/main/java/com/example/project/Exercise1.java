32ca14533de17c91a19218a3800a849f32ff5af6
Eberth <88208531+eazurin@users.noreply.github.com>
2022-05-04 13:27:29 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -15,8 +15,43 @@ public class Exercise1 {
 	}
 
 	public String convertirBaseGabriel(int a) {
-
-		// TO DO
-		return "El numero proporcionado no esta en base Gabriel.";
+		String aCadena = a + ""; 
+		int [] misCifras = new int [aCadena.length()];
+		for (int i = 0; i<aCadena.length(); i++) {
+			misCifras[i] = Integer.parseInt(aCadena.charAt(i)+"");
+		}
+		if(verificarBaseGabriel(misCifras)) {
+			int resultado = 0;
+			for (int i = 0; i<aCadena.length(); i++) {
+				resultado = (int)(resultado + misCifras[i]*(Math.pow(2, aCadena.length() - i) - 1));
+			}
+			return resultado+"";
+		}
+		else
+			return "El numero proporcionado no esta en base Gabriel.";
+	}
+	public boolean verificarBaseGabriel(int [] b) {
+		int contadorDos = 0;
+		int contadorMayor = 0;
+		for (int i = 0; i<b.length; i++) {
+			if (b[i] == 2)
+				contadorDos++;
+			else if (b[i] > 2)
+				contadorMayor++;
+		}
+		if(contadorDos < 2 && contadorMayor == 0) {
+			if(contadorDos == 0)
+				return true;
+			else {
+				for (int i = 0; i<b.length; i++) {
+					if (b[i] == 2 && b[i+1] == 0) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 	}
 }
+
+
